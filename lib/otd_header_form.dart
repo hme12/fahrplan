e243d3f3fffdcdf0a39de3bcf6bd61b736cf9422
hme12 <hme12@az8by.ch>
2025-12-10 22:20:06 +0100

    Add files via upload

---
+++ lib/otd_header_form.dart
@@ -0,0 +1,11 @@
+Map<String, String> myFormHeader() {
+  Map<String, String> header = {
+    'accept': '*/*',
+    'User-Agent': 'Mozilla/5.0',
+    'Authorization':
+        'Bearer your formation api key',
+    'Content-Type': 'application/xml'
+  };
+
+  return header;
+}
