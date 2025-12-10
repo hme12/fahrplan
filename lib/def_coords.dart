5c1246cf905c4160b54f714cd230183a64507528
hme12 <hme12@az8by.ch>
2025-12-10 22:23:02 +0100

    Add files via upload

--- lib/def_coords.dart
+++ lib/def_coords.dart
@@ -1,4 +1,2 @@
-double defLatitude = 46.777;
-//  double defLatitude = 47.36652;
-double defLongitude = 10.1481;
-//  double defLongitude = 8.54095;
+double defLatitude = 47.36652;
+double defLongitude = 8.54095;
